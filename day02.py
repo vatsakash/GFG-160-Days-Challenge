82b3bdd25e11cd2cad82df14f94b3969b5bbe830
Akash Vats <vats9353@gmail.com>
2026-05-31 00:22:25 +0530

    Add files via upload

---
+++ day02.py
@@ -0,0 +1,15 @@
+def pushZerosToEnd(arr):
+    # code here
+    count = 0
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[count] = arr[i]
+            count += 1
+    while count < len(arr):
+        arr[count] = 0
+        count += 1
+    
+    return arr
+        
+arr = [1, 2, 0, 4, 3, 0, 5, 0]        
+pushZerosToEnd(arr)
